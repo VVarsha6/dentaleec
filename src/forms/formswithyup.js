7f7d4dad1dda0cadc4d3d503a6a26db4766dcbab
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-03-31 23:18:47 +0530

    refactor: forms accessibility

---
+++ src/forms/formswithyup.js
@@ -0,0 +1,61 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const inputSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required('Please enter name')
+    .matches(/^[aA-zZ\s]+$/, 'Only alphabets are allowed for this field '),
+  age: yup
+    .number()
+    .required()
+    .positive('enter valid age'),
+  // dob: yup
+  //   .date()
+  //   .required('Date is required')
+  //   .test('DOB', 'Please choose a valid date of birth', value => {
+  //     return moment().diff(moment(value), 'years') >= 18;
+  //   }),
+  checkbox: yup.boolean().oneOf([true], 'unchecked'),
+  dropdown: yup
+    .string()
+    .required('Please indicate your communications preference'),
+});
+const FormWithYup = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ resolver: yupResolver(inputSchema) });
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='organizer'>Organizer</label>
+      <input id="organizer" {...register('name')} type="text" placeholder='enter name' />
+      <p>{errors.name?.message}</p>
+
+      <label htmlFor='dropdown'>Dropdown</label>
+      <select id="dropdown" {...register('dropdown')}  >
+        <option value="" disabled selected hidden>--Please choose an option--</option>
+        <option value="d1">opt1</option>
+        <option value="d2">opt2</option>
+        <option value="d3">opt3</option>
+      </select>
+      <p>{errors.dropdown?.message}</p>
+
+      <p>Patient Entry</p>
+      <label htmlFor="patient-age">age:</label>
+      <input id="patient-age" {...register('age')} placeholder="enter age" />
+      <p>{errors.age?.message}</p>
+
+      <label htmlFor='chk-outpatient'>Outpatient</label>
+      <input id="chk-outpatient" {...register('checkbox')} type="checkbox" />
+      <p>{errors.checkbox?.message}</p>
+      <button type="submit">Submit</button>
+    </form>
+
+
+  )
+};
+export default FormWithYup;
