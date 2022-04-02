76729c4b0833d270d861ed6221e8303ef2e4a707
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-04-02 15:29:21 +0530

    style: form template

--- src/forms/FormWithYup.js
+++ src/forms/FormWithYup.js
@@ -28,28 +28,34 @@ const FormWithYup = () => {
   return (
     <section className='form-section'>
       <form className='form-camp' onSubmit={handleSubmit(onsubmit)}>
-        <label htmlFor='organizer'>Organizer</label>
-        <input id="organizer" {...register('name')} type="text" placeholder='enter name' />
+        <div className='form-element'>
+          <label className='form-label' htmlFor='organizer'>Organizer</label>
+          <input id="organizer" {...register('name')} type="text" placeholder='enter name' />
+        </div>
         <p className='yup_error'>{errors.name?.message}</p>
-
-        <label htmlFor='dropdown'>Dropdown</label>
-        <select id="dropdown" {...register('dropdown')}  >
-          <option value="" disabled selected hidden>--Please choose an option--</option>
-          <option value="d1">opt1</option>
-          <option value="d2">opt2</option>
-          <option value="d3">opt3</option>
-        </select>
+        <div className='form-element'>
+          <label className='form-label' htmlFor='dropdown'>Dropdown</label>
+          <select id="dropdown" {...register('dropdown')}  >
+            <option value="" disabled selected hidden>--Please choose an option--</option>
+            <option value="d1">opt1</option>
+            <option value="d2">opt2</option>
+            <option value="d3">opt3</option>
+          </select>
+        </div>
         <p className='yup_error'>{errors.dropdown?.message}</p>
-
-        {/* <p className='form-subtitle'>Patient Entry</p> */}
-        <label htmlFor="patient-age">Age</label>
-        <input id="patient-age" {...register('age')} placeholder="enter age" />
+        <p className='form-subtitle'>Patient Entry</p>
+        <div className='form-element'>
+          <label className='form-label' htmlFor="patient-age">Age</label>
+          <input id="patient-age" {...register('age')} placeholder="enter age" />
+        </div>
         <p className='yup_error'>{errors.age?.message}</p>
+        <div className='form-element' >
+          <label className='form-label' htmlFor='chk-outpatient'>Outpatient</label>
+          <input className='chkbox' id="chk-outpatient" {...register('checkbox')} type="checkbox" />
+        </div>
 
-        <label htmlFor='chk-outpatient'>Outpatient</label>
-        <input id="chk-outpatient" {...register('checkbox')} type="checkbox" />
         {/* <p className='yup_error'>{errors.checkbox?.message}</p> */}
-        <input type="submit" />
+        <button className='center-hori' type="submit">SUBMIT</button>
       </form>
 
     </section>
