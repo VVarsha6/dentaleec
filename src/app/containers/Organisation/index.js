0f6ed15a0f2877b461008e95ca2ee6be37a3fe9e
Varsha Viswanathan <varshaviswanathan6@gmail.com>
2022-05-05 12:47:36 +0530

    add: table (org)

--- src/app/containers/Organisation/index.js
+++ src/app/containers/Organisation/index.js
@@ -5,7 +5,7 @@
  */
 
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Table } from "reactstrap";
 import OrganisationForm from "../../components/OrganisationForm";
 import SidebarLayout from "../../components/SidebarLayout";
 import TitleBar from "../../components/TitleBar";
@@ -16,12 +16,34 @@ export default function Organisation() {
     <Container fluid className="mainLayout m-3">
       <TitleBar />
       <Row className="subLayout">
-        <Col lg="2" md="3" xs="4" className="subsLayout">
+        <Col xs="3" className="subsLayout">
           <SidebarLayout />
         </Col>
-        <Col className="formLayout">
-          <p className="primaryHeading">ORGANISATION ENTRY</p>
-          <OrganisationForm />
+        <Col xs="9" className="formLayout">
+          <p className="primaryHeading">ORGANISATION</p>
+          <Table>
+            <thead>
+              <tr>
+              <th>
+                S.No.
+              </th>
+              <th>
+                Organisation Name
+              </th>
+              <th>
+                Organiser Name
+              </th>
+              <th>
+                Address
+              </th>
+              <th>
+                Ph.No.
+              </th>
+              <th>
+              </th>
+              </tr>
+            </thead>
+          </Table>
         </Col>
       </Row>
     </Container>
